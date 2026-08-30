b623cc693910f2e0c818d147b2a114004691c8c6
vitalii_terentev_suchkov <tevvit@gmail.com>
2026-08-30 11:45:15 +0300

    The system theme has been retrieved and the required classes have been applied.

---
+++ src/utils/getSystemTheme.ts
@@ -0,0 +1,7 @@
+import { Theme } from "@/types/poker";
+
+export function getSystemTheme(): Theme {
+  if (typeof window === "undefined") return "light";
+
+  return window.matchMedia("(prefers-color-scheme: dark)").matches ? "navy" : "light";
+}
